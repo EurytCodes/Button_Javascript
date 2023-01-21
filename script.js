83777b2417c3f27c8518f16a7c85039f6b61c1f5
euryt204 <oppongagyeieugene@gmail.com>
2023-01-21 10:58:52 +0000

    Update on the Click event

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ const mainbutton = document.getElementById('mainBtn');
 const circlebutton = document.getElementById('circleBtn');
 
 circlebutton.addEventListener('click', On);
-circlebutton.addEventListener('dblclick', Off);
 
+var state = 0;
 
-function On() {
-    circlebutton.style.float = 'right';
-    circlebutton.style.backgroundColor = 'black';
-    circlebutton.style.color = 'white';
-    circlebutton.innerText = 'ON';
-    mainbutton.style.backgroundColor = 'white';
-    circlebutton.style.border = '2px solid white';
-}
-
-function Off() {
-    circlebutton.style.float = 'left';
-    circlebutton.innerText = 'OFF';
-    circlebutton.style.backgroundColor = 'white';
-    circlebutton.style.color = 'black';
-    circlebutton.style.border = '2px black solid';
-    mainbutton.style.backgroundColor = 'black';
+function On(){
+    if(state == 0){
+        circlebutton.style.float = 'right';
+        circlebutton.style.backgroundColor = 'black';
+        circlebutton.style.color = 'white';
+        circlebutton.innerText = 'ON';
+        mainbutton.style.backgroundColor = 'white';
+        circlebutton.style.border = '2px solid white';
+        state = 1;
+    }
+    else{
+        circlebutton.style.float = 'left';
+        circlebutton.innerText = 'OFF';
+        circlebutton.style.backgroundColor = 'white';
+        circlebutton.style.color = 'black';
+        circlebutton.style.border = '2px black solid';
+        mainbutton.style.backgroundColor = 'black';
+        state = 0;
+    }
 }
